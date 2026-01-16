e88d69984329b729352faa995f186a9ab7b0c637
Marie <marie.lignoncoscioli@edu.ece.fr>
2026-01-16 12:18:00 +0100

    initial commit

---
+++ projet0/bin/projet0.dart
@@ -0,0 +1,5 @@
+import 'package:projet0/projet0.dart' as projet0;
+
+void main(List<String> arguments) {
+  print('Hello world: ${projet0.calculate()}!');
+}
---
+++ projet0/test/projet0_test.dart
@@ -0,0 +1,8 @@
+import 'package:projet0/projet0.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
